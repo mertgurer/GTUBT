a0ae8f2e3f2b3cd00401281c326ebf9e4979558c
Mert Gürer <mert.gurer@hotmail.com.tr>
2025-02-26 09:56:57 +0300

    update

--- src/components/Aboutus/Aboutus.tsx
+++ src/components/Aboutus/Aboutus.tsx
@@ -5,52 +5,49 @@ import { Link } from "react-router-dom";
 import { DataContext } from "../../App";
 
 const Aboutus = () => {
-    const { mobile } = useContext(DataContext);
+  const { mobile } = useContext(DataContext);
 
-    return (
-        <section id="aboutus" className="aboutus-container">
-            <div className="aboutus-zone left">
-                <img src={logo} alt="aboutus" className="aboutus-image" />
-            </div>
-            <div className="aboutus-zone right">
-                <p className="aboutus-header">Biz Kimiz?</p>
-                <p className="aboutus-text">
-                    Teknolojiyi seven ve sevdirmeye çalışan bir topluluğuz.{" "}
-                    <span className="aboutus-highlight">
-                        GTU Bilgisayar Topluluğu
-                    </span>{" "}
-                    olarak 2016 yılından bu yana her seni üye sayısını artırarak
-                    daha çok kişiye teknolojiyi sevdirmeyi amaçlıyoruz.
-                    Öğrenmenin bulaşıcı olduğuna inanıyoruz. Öğrendiklerimizi
-                    birbirimize öğretiyoruz. <br />
-                    <br />
-                    Topluluğun daha geniş kitlelere yayılmasını ve hep beraber
-                    geliştirmeyi istiyoruz.{" "}
-                    <span className="aboutus-highlight">
-                        ETKİNLİKLERİMİZ ve ATÖLYELERİMİZ
-                    </span>{" "}
-                    ile meraklısı olduğumuz konuları birinci ağızdan duyup,
-                    sektörün usta isimleriyle birebir iletişime geçiyoruz.
-                </p>
-                {!mobile && (
-                    <Link
-                        to={"/geekday"}
-                        className="navbar-special"
-                        style={{
-                            alignSelf: "center",
-                            marginTop: 12,
-                            paddingLeft: 28,
-                            paddingRight: 28,
-                            paddingTop: 16,
-                            paddingBottom: 16,
-                        }}
-                    >
-                        GeekDay 2024!
-                    </Link>
-                )}
-            </div>
-        </section>
-    );
+  return (
+    <section id="aboutus" className="aboutus-container">
+      <div className="aboutus-zone left">
+        <img src={logo} alt="aboutus" className="aboutus-image" />
+      </div>
+      <div className="aboutus-zone right">
+        <p className="aboutus-header">Biz Kimiz?</p>
+        <p className="aboutus-text">
+          Teknolojiyi seven ve sevdirmeye çalışan bir topluluğuz.{" "}
+          <span className="aboutus-highlight">GTU Bilgisayar Topluluğu</span>{" "}
+          olarak 2016 yılından bu yana her seni üye sayısını artırarak daha çok
+          kişiye teknolojiyi sevdirmeyi amaçlıyoruz. Öğrenmenin bulaşıcı
+          olduğuna inanıyoruz. Öğrendiklerimizi birbirimize öğretiyoruz. <br />
+          <br />
+          Topluluğun daha geniş kitlelere yayılmasını ve hep beraber
+          geliştirmeyi istiyoruz.{" "}
+          <span className="aboutus-highlight">
+            ETKİNLİKLERİMİZ ve ATÖLYELERİMİZ
+          </span>{" "}
+          ile meraklısı olduğumuz konuları birinci ağızdan duyup, sektörün usta
+          isimleriyle birebir iletişime geçiyoruz.
+        </p>
+        {!mobile && (
+          <Link
+            to={"/geekday"}
+            className="navbar-special"
+            style={{
+              alignSelf: "center",
+              marginTop: 12,
+              paddingLeft: 28,
+              paddingRight: 28,
+              paddingTop: 16,
+              paddingBottom: 16,
+            }}
+          >
+            GeekDay 2025!
+          </Link>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default Aboutus;
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -6,50 +6,50 @@ import { Link } from "react-router-dom";
 import { DataContext } from "../../App";
 
 function Home() {
-    const { mobile } = useContext(DataContext);
+  const { mobile } = useContext(DataContext);
 
-    return (
-        <section id="home" className="home-container">
-            <div className="home-zone">
-                <div className="home-zone-left">
-                    <h1 className="heading">
-                        Hoşgeldin!{" "}
-                        <span className="wave" role="img">
-                            👋🏻
-                        </span>
-                    </h1>
+  return (
+    <section id="home" className="home-container">
+      <div className="home-zone">
+        <div className="home-zone-left">
+          <h1 className="heading">
+            Hoşgeldin!{" "}
+            <span className="wave" role="img">
+              👋🏻
+            </span>
+          </h1>
 
-                    <h1 className="heading-name">
-                        BİZ GTU
-                        <strong className="heading-name-main">
-                            {" "}
-                            BİLGİSAYAR TOPLULUĞUYUZ
-                        </strong>
-                    </h1>
-                    <Type />
-                    {mobile && (
-                        <Link
-                            to={"/geekday"}
-                            className="navbar-special"
-                            style={{
-                                alignSelf: "flex-start",
-                                marginTop: 12,
-                                paddingLeft: 28,
-                                paddingRight: 28,
-                                paddingTop: 16,
-                                paddingBottom: 16,
-                            }}
-                        >
-                            GeekDay 2024!
-                        </Link>
-                    )}
-                </div>
-                <div className="home-image-box">
-                    <img src={homeLogo} alt="home" className="home-image" />
-                </div>
-            </div>
-        </section>
-    );
+          <h1 className="heading-name">
+            BİZ GTU
+            <strong className="heading-name-main">
+              {" "}
+              BİLGİSAYAR TOPLULUĞUYUZ
+            </strong>
+          </h1>
+          <Type />
+          {mobile && (
+            <Link
+              to={"/geekday"}
+              className="navbar-special"
+              style={{
+                alignSelf: "flex-start",
+                marginTop: 12,
+                paddingLeft: 28,
+                paddingRight: 28,
+                paddingTop: 16,
+                paddingBottom: 16,
+              }}
+            >
+              GeekDay 2025!
+            </Link>
+          )}
+        </div>
+        <div className="home-image-box">
+          <img src={homeLogo} alt="home" className="home-image" />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Home;
